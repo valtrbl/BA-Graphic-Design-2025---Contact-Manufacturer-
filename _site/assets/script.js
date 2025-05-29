57eac08b29fc568d383c9d6641c9d9c8eae6eb7e
Valinnie Crasto <valinniecrasto@gmail.com>
2025-05-29 15:38:16 +0100

    First edits

---
+++ _site/assets/script.js
@@ -0,0 +1,26 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+
+// function toggleMenu() {
+//   const menu = document.querySelector('.tab-links');
+//   menu.classList.toggle('active');
+// }
+
+// const hamMenu = document.querySelector('.ham-menu');
+
+// const offScreenMenu = document.querySelector('.off-screen-menu');
+
+// hamMenu.addEventListener('click', () => {
+//   hamMenu.classList.toggle('active');
+//   offScreenMenu.classList.toggle('active');
+// })
+
+
+
+
